cbdb3f37e06fa6a5831147371dba19550c1db2c1
gellel <lindsay.gelle@gmail.com>
2019-11-02 19:06:10 +1100

    Added constructor function for mixAmiiboMap

--- mixAmiiboMap.go
+++ mixAmiiboMap.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 type mixAmiiboMap map[string]*mixAmiibo
@@ -32,4 +33,24 @@ func newMixAmiiboMap(c *compatabilityAmiiboMap, l *lineupAmiiboMap) (*mixAmiiboM
 		}
 	}
 	return &m, err
+}
+
+func newMixAmiiboMapFromMix(m *mix) (*mixAmiiboMap, error) {
+	var (
+		c  *compatabilityAmiiboMap
+		l  *lineupAmiiboMap
+		wg sync.WaitGroup
+	)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		c, _ = newCompatabilityAmiiboMap(m.CompatabilityAmiibo)
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		l, _ = newLineupAmiiboMap(m.LineupAmiibo)
+	}()
+	wg.Wait()
+	return newMixAmiiboMap(c, l)
 }
