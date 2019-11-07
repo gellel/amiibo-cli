527996660223296e870d342d3e6c93fdda3fab56
gellel <lindsay.gelle@gmail.com>
2019-11-07 19:40:59 +1100

    Added constructor for mixGameMap

--- mixGameMap.go
+++ mixGameMap.go
@@ -1,3 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 type mixGameMap map[string]*mixGame
+
+func newMixGameMap(c *compatabilityGameMap, i *compatabilityItemMap) (*mixGameMap, error) {
+	var (
+		err error
+		m   = mixGameMap{}
+		ok  bool
+
+		mu sync.Mutex
+		wg sync.WaitGroup
+	)
+	ok = (c != nil)
+	if !ok {
+		return nil, fmt.Errorf("*c is nil")
+	}
+	ok = (i != nil)
+	if !ok {
+		return nil, fmt.Errorf("*i is nil")
+	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for k, v := range *c {
+			mu.Lock()
+			if _, ok := m[k]; !ok {
+				m[k] = &mixGame{}
+			}
+			x := m[k]
+			x.compatabilityGame = v
+			mu.Unlock()
+		}
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for k, v := range *i {
+			mu.Lock()
+			if _, ok := m[k]; !ok {
+				m[k] = &mixGame{}
+			}
+			x := m[k]
+			x.compatabilityItem = v
+			mu.Unlock()
+		}
+	}()
+	wg.Wait()
+	return &m, err
+}
